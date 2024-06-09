f1dbd0341f0bf3d19c0a07dd31de3b8383369470
Daivik <daivikkarbhari01@gmail.com>
2024-06-09 20:54:58 +0530

    update .gitignore and bindings structure

--- python_bindings/bindings.cpp
+++ python_bindings/bindings.cpp
@@ -1,12 +1,10 @@
-#pragma once
 #include "../subprojects/seldon/include/models/DeGroot.hpp"
 #include "../subprojects/seldon/include/agents/simple_agent.hpp"
 #include "../subprojects/seldon/include/config_parser.hpp"
 #include "../subprojects/seldon/include/model.hpp"
 #include "../subprojects/seldon/include/network.hpp"
 
-
-//pybind11 headers
+// pybind11 headers
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl/filesystem.h>
@@ -16,8 +14,24 @@ using namespace std::string_literals;
 using namespace pybind11::literals;
 namespace py = pybind11;
 
+
 PYBIND11_MODULE(seldoncore, m)
 {
     m.doc() = "Python bindings for Seldon Cpp Engine";
-    
+
+//Agents
+
+//Config-Parser
+
+//Network
+
+//Simulation
+
+
+// Models
+
+    //DeGroot 
+    py::class_<Seldon::DeGrootModel>( m, "DeGrootModel" )
+        .def(py::init<>()) //Config::DeGrootSettings settings, NetworkT & network 
+        
 }
