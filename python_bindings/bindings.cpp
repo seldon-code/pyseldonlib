0af82a857e88ee5e480e61cf5fdff396cd2bbd7a
Daivik <daivikkarbhari01@gmail.com>
2024-06-09 12:57:20 +0530

    starting with bindings

--- python_bindings/bindings.cpp
+++ python_bindings/bindings.cpp
@@ -1,1 +1,23 @@
-#include "seldon/include/"
+#pragma once
+#include "../subprojects/seldon/include/models/DeGroot.hpp"
+#include "../subprojects/seldon/include/agents/simple_agent.hpp"
+#include "../subprojects/seldon/include/config_parser.hpp"
+#include "../subprojects/seldon/include/model.hpp"
+#include "../subprojects/seldon/include/network.hpp"
+
+
+//pybind11 headers
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <pybind11/stl/filesystem.h>
+#include <pybind11/operators.h>
+
+using namespace std::string_literals;
+using namespace pybind11::literals;
+namespace py = pybind11;
+
+PYBIND11_MODULE(seldoncore, m)
+{
+    m.doc() = "Python bindings for Seldon Cpp Engine";
+    
+}
