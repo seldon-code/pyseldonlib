9663f5fded3502b83367a6b02095cd98ecfe587c
Daivik <daivikkarbhari01@gmail.com>
2024-06-11 00:52:49 +0530

    getting errors with respect to fmt

--- python_bindings/bindings.cpp
+++ python_bindings/bindings.cpp
@@ -1,8 +1,9 @@
-#include "../subprojects/seldon/include/models/DeGroot.hpp"
-#include "../subprojects/seldon/include/agents/simple_agent.hpp"
-#include "../subprojects/seldon/include/config_parser.hpp"
-#include "../subprojects/seldon/include/model.hpp"
-#include "../subprojects/seldon/include/network.hpp"
+#include "config_parser.hpp"
+#include "model.hpp"
+#include "network.hpp"
+#include "run_model.hpp"
+
+
 
 // pybind11 headers
 #include <pybind11/pybind11.h>
@@ -31,7 +32,15 @@ PYBIND11_MODULE(seldoncore, m)
 // Models
 
     //DeGroot 
-    py::class_<Seldon::DeGrootModel>( m, "DeGrootModel" )
-        .def(py::init<>()) //Config::DeGrootSettings settings, NetworkT & network 
-        
+    // py::class_<Seldon::DeGrootModel>( m, "DeGrootModel" )
+    //     .def(py::init<>(Seldon::Config::DeGrootSettings, Seldon::)) //->takes Config::DeGrootSettings settings, NetworkT & network 
+
+// Main wrapper
+// use this to achieve complete abstration of whats happening underneath ;)
+// but added include/main.hpp for the compiler to indentify this
+    m.def("run_model", &run_model ,"The Wrapper for running the model",
+        py::arg("config_file_path"),
+        py::arg("agent_file") = "",
+        py::arg("network_file") = "",
+        py::arg("output_dir_path") = "./output");
 }
