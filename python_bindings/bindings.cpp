773eeeb0410ec2d487cd2a5dda94d98ef15225d7
Daivik <daivikkarbhari01@gmail.com>
2024-06-17 20:24:26 +0530

    ENH: Started with Network class bindings

--- python_bindings/bindings.cpp
+++ python_bindings/bindings.cpp
@@ -22,81 +22,28 @@ using namespace pybind11::literals;
 namespace py = pybind11;
 
 
-namespace Seldon::Config{
-
-    SimulationOptions parse_options_parameters(std::optional<std::string> model_string,std::optional int rng_seed,std::optional bool print_progress, std::optional bool output_initial,
-    std::optional size_t start_output, std::optional size_t start_numbering_from, std::optional<size_t> n_output_agents, std::optional<size_t> n_output_network ){
-         SimulationOptions options;
-
-    options.rng_seed = rng_seed.value_or( int( options.rng_seed ) );
-
-    // Parse output settings
-    options.output_settings.n_output_network = n_output_network;
-    options.output_settings.n_output_agents  = n_output_agents;
-    options.output_settings.print_progress = print_progress.value_or( options.output_settings.print_progress );
-    output_settings.output_initial = output_initial.value_or( options.output_settings.output_initial );
-    output_settings.start_output = start_output.value_or( options.output_settings.start_output );
-    output_settings.start_numbering_from = start_numbering_from.value_or( options.output_settings.start_numbering_from );
-    
-    // Check if the 'model' keyword exists
-    if( !model_string.has_value() )
-        throw std::runtime_error( fmt::format( "Configuration file needs to include 'simulation.model'!" ) );
-
-    options.model_string = model_string.value();
-    options.model        = model_string_to_enum( model_string.value() );
-
-    if( options.model == Model::DeGroot )
-    {
-        auto model_settings           = DeGrootSettings();
-        model_settings.max_iterations = tbl["model"]["max_iterations"].value<int>();
-        set_if_specified( model_settings.convergence_tol, tbl[options.model_string]["convergence"] );
-        options.model_settings = model_settings;
-    }
-    else if( options.model == Model::DeffuantModel )
-    {
-        auto model_settings           = DeffuantSettings();
-        model_settings.max_iterations = tbl["model"]["max_iterations"].value<int>();
-        set_if_specified( model_settings.homophily_threshold, tbl[options.model_string]["homophily_threshold"] );
-        set_if_specified( model_settings.mu, tbl[options.model_string]["mu"] );
-        set_if_specified( model_settings.use_network, tbl[options.model_string]["use_network"] );
-        // Options for the DeffuantModelVector model
-        set_if_specified( model_settings.use_binary_vector, tbl[options.model_string]["binary_vector"] );
-        set_if_specified( model_settings.dim, tbl[options.model_string]["dim"] );
-        options.model_settings = model_settings;
-    }
-    else if( options.model == Model::ActivityDrivenModel )
-    {
-        auto model_settings = ActivityDrivenSettings();
-
-        parse_activity_settings( model_settings, tbl[options.model_string], tbl );
-        options.model_settings = model_settings;
-    }
-    else if( options.model == Model::ActivityDrivenInertial )
-    {
-        auto model_settings = ActivityDrivenInertialSettings();
-
-        parse_activity_settings( model_settings, tbl[options.model_string], tbl );
-        set_if_specified( model_settings.friction_coefficient, tbl[options.model_string]["friction_coefficient"] );
-        options.model_settings = model_settings;
-    }
-
-    // Parse settings for the generation of the initial network
-    options.network_settings = InitialNetworkSettings();
-    set_if_specified( options.network_settings.n_agents, tbl["network"]["number_of_agents"] );
-    set_if_specified( options.network_settings.n_connections, tbl["network"]["connections_per_agent"] );
-
-    return options;
-    }
-}
-
-
 PYBIND11_MODULE(seldoncore, m)
 {
     m.doc() = "Python bindings for Seldon Cpp Engine";
 
 //Agents
 
+//Network
+//exposing the network class so that we can directly create a model using the nework and run the simulation
+
+    // py::class_<Seldon::Network>(m, "Network")
+    //     .def(py::init<>())
+    //     .def(py::init<const std::size_t>())
+    //     .def(py::init<const std::vector<int> &>())//int
+    //     .def(py::init<const std::vector<double> &>()) //double ->AgentT
+    //     .def(py::init<const std::vector<std::vector<size_t>> &&, const std::vector<std::vector<double>> &&>()) //using WeightT->double
+    //     .def();
+
+
+
 //Config-Parser
+
+    // So here the basic approach is making an object of each setting passing it to the SimulationOptions and also making an object of Simulation options and passing it to the Simulation class constructor
     //enum class Model (might require to change this name as its a todo in the seldon codebase )
     py::enum_<Seldon::Config::Model>(m, "Model")
         .value("DeGroot", Seldon::Config::Model::DeGroot, "DeGroot Model")
@@ -167,7 +114,7 @@ PYBIND11_MODULE(seldoncore, m)
         .def_readwrite("n_agents", &Seldon::Config::InitialNetworkSettings::n_agents)
         .def_readwrite("n_connections", &Seldon::Config::InitialNetworkSettings::n_connections);
 
-    //SimulationOptions object to be passed in the simulation class
+    //SimulationOptions class object creation which is to be passed in the simulation class
     py::class_<Seldon::Config::SimulationOptions>(m, "SimulationOptions")
         .def(py::init<>())
         .def_readwrite("model_string", &Seldon::Config::SimulationOptions::model_string)
@@ -178,27 +125,18 @@ PYBIND11_MODULE(seldoncore, m)
         .def_readwrite("network_settings", &Seldon::Config::SimulationOptions::network_settings);
     
 
-
-//Network
-
-
-//Model enum used in simulation options
-    // py::enum_<Seldon::Config::Model>(m, "Model")
-    //     .value("DeGroot")
-        
-    
-    
 //Simulation class
-    //this is a simulations options object which can be passed in the Simulation class constructor
-    py::class_<Seldon::Config::SimulationOptions>(m, "SimulationOptions")
-        .def(py::init<>())
-        .def_readwrite();
-
     py::class_<Seldon::Simulation, Seldon::SimulationInterface>(m , "Simulation")
         .def(py::init<Config::SimulationOptions &, const std::optional<std::string &, const std::optional<std::string> &>())
         .def("run", &Seldon::Simulation::run, "Run the Simulation",
         py::arg("output_dir_path" = "./output")
-        );
+        )
+        .def("create_network", &Seldon::Simulation::create_network, "Create the network") //exposing it here so that it can be used to create a network file
+        .def("create_model", &Sledon::Simulation::create_model, "Create the model") //exposing it here so that it can be used to create a model according to the simulation options
+
+    
+
+
 
 
 // Models
@@ -219,10 +157,7 @@ PYBIND11_MODULE(seldoncore, m)
     // Assuming <AgentType> as int and double only
     //int
     // py::class_<Seldon::DeGrootModel, Seldon::Model<int>>( m, "DeGrootModel" )
-    //     .def(py::init<Seldon::Config::DeGrootSettings, Seldon::Network &>())
-    //     .def_readwrite("convergence_tol", &Seldon::DeGrootModel::convergence_tol)
-    //     .def_readwrite("network", &Seldon::DeGrootModel::network)
-    //     .def_readwrite("agents_current_copy", &Seldon::DeGrootModel::agents_current_copy);
+    //     .def(py::init<Seldon::Config::DeGrootSettings&, Seldon::Config::Netw>())
 
     // //double
     //  py::class_<Seldon::DeGrootModel, Seldon::Model<double>>( m, "DeGrootModel" )
