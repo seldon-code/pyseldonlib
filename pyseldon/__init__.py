7914046a78b67f33f3911281cdcbc980e504e65b
Daivik <daivikkarbhari01@gmail.com>
2024-07-07 14:34:49 +0530

    Getting errors

--- pyseldon/__init__.py
+++ pyseldon/__init__.py
@@ -1,3 +1,3 @@
-from pyseldon import seldoncore
+from pyseldon.simulation import run_simulation_from_config_file, run_simulation_from_options
 
-from .simulation import run_simulation_from_config_file, run_simulation_from_options
+__all__ = ["run_simulation_from_config_file", "run_simulation_from_options"]
--- pyseldon/simulation.py
+++ pyseldon/simulation.py
@@ -1,6 +1,5 @@
 """This module provides functions to run the simulation using the configuration file or the simulation options object."""
-
-import pyseldon.seldoncore 
+import pyseldon.core as core
 
 def run_simulation_from_config_file(config_file_path, agent_file_path = None, network_file_path = None, output_file_path = None):
     """Run the simulation using the configuration file.
@@ -19,8 +18,7 @@ def run_simulation_from_config_file(config_file_path, agent_file_path = None, ne
     output_file_path : str
         The path to the output file.
     """
-    
-    pyseldon.seldoncore.run_simulation(config_file_path = config_file_path, agent_file_path = agent_file_path, network_file_path = network_file_path, output_file_path = output_file_path)
+    core.seldoncore.run_simulation(config_file_path = config_file_path, agent_file_path = agent_file_path, network_file_path = network_file_path, output_file_path = output_file_path)
 
 
 def run_simulation_from_options(options, agent_file_path = None, network_file_path = None, output_file_path = None):
@@ -40,5 +38,4 @@ def run_simulation_from_options(options, agent_file_path = None, network_file_pa
     output_file_path : str
         The path to the output file.
     """
-    
-    pyseldon.seldoncore.run_simulation(options = options, agent_file_path = agent_file_path, network_file_path = network_file_path, output_file_path = output_file_path)
+    core.seldoncore.run_simulation(options = options, agent_file_path = agent_file_path, network_file_path = network_file_path, output_file_path = output_file_path)
